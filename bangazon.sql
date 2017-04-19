af97ed615270efb8c384b28c3beeb847e7c1421f
Jeremy Bakker <jeremybakker@Jeremys-MacBook-Pro.local>
2017-04-19 10:36:44 -0500

    fixed spacing on .sql

--- bangazon.sql
+++ bangazon.sql
@@ -24,14 +24,14 @@ DROP TABLE IF EXISTS ProductOrder;
 
 CREATE TABLE Computer (
     ComputerId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	Description TEXT NOT NULL,
+    Description TEXT NOT NULL,
     PurchaseDate TEXT NOT NULL,
     DecomissionDate TEXT NOT NULL
 );
 
 CREATE TABLE PaymentTypes (
     PaymentTypeId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	PaymentType TEXT NOT NULL,
+    PaymentType TEXT NOT NULL,
     Account Number INT NOT NULL
 );
 
@@ -43,70 +43,70 @@ CREATE TABLE ProductTypes (
 CREATE TABLE Training (
     TrainingId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     TrainingTitle TEXT NOT NULL,
-	StartDate TEXT NOT NULL,
+    StartDate TEXT NOT NULL,
     EndDate TEXT NOT NULL,
-	MaxAttendees TEXT NOT NULL,
-	EmployeeTrainingId INT NOT NULL,
-	FOREIGN KEY(EmployeeTrainingId) REFERENCES EmployeeTraining(EmployeeTrainingId)
+    MaxAttendees TEXT NOT NULL,
+    EmployeeTrainingId INT NOT NULL,
+    FOREIGN KEY(EmployeeTrainingId) REFERENCES EmployeeTraining(EmployeeTrainingId)
 );
 
 CREATE TABLE EmployeeTraining (
     EmployeeTrainingId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     EmployeeId INT NOT NULL,
-	FOREIGN KEY (EmployeeId) REFERENCES Employees(EmployeeId)
+    FOREIGN KEY (EmployeeId) REFERENCES Employees(EmployeeId)
 );
 
 CREATE TABLE Employees (
     EmployeeId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     FirstName TEXT NOT NULL,
     LastName TEXT NOT NULL,
-	DepartmentId INT NOT NULL,
-	ComputerId INT NOT NULL,
-	Supervisor TEXT NOT NULL,
-	FOREIGN KEY(ComputerId) REFERENCES Computer(ComputerId),
-	FOREIGN KEY(DepartmentId) REFERENCES Departments(DepartmentId)
+    DepartmentId INT NOT NULL,
+    ComputerId INT NOT NULL,
+    Supervisor TEXT NOT NULL,
+    FOREIGN KEY(ComputerId) REFERENCES Computer(ComputerId),
+    FOREIGN KEY(DepartmentId) REFERENCES Departments(DepartmentId)
 );
 
 
 CREATE TABLE Orders (
     OrderId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	CustomerId INT NOT NULL,
-	PaymentTypeId INT NOT NULL,
+    CustomerId INT NOT NULL,
+    PaymentTypeId INT NOT NULL,
     ProductOrderId INT NOT NULL,
-	Completed TEXT NOT NULL,
-	FOREIGN KEY(CustomerId) REFERENCES Customers(CustomerId),
-	FOREIGN KEY(ProductOrderId) REFERENCES ProductOrder(ProductOrderId)
+    Completed TEXT NOT NULL,
+    FOREIGN KEY(CustomerId) REFERENCES Customers(CustomerId),
+    FOREIGN KEY(ProductOrderId) REFERENCES ProductOrder(ProductOrderId)
 );
 
 CREATE TABLE ProductOrder (
     ProductOrderId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	ProductId INT NOT NULL,
-	FOREIGN KEY(ProductId) REFERENCES ProductOrder(ProductId)
+    ProductId INT NOT NULL,
+    FOREIGN KEY(ProductId) REFERENCES ProductOrder(ProductId)
 );
 
 CREATE TABLE Departments (
     DepartmentId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	Name TEXT NOT NULL,
-	Budget TEXT NOT NULL
+    Name TEXT NOT NULL,
+    Budget TEXT NOT NULL
 );
 
 CREATE TABLE Products (
     ProductId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	ProductTypeId INT NOT NULL,
-	Price INT NOT NULL,
-	Name TEXT NOT NULL,
-	Description TEXT NOT NULL,
-	CustomerId INT NOT NULL,
-	FOREIGN KEY(CustomerId) REFERENCES Customers(CustomerId),
-	FOREIGN KEY(ProductTypeId) REFERENCES ProductTypes(ProductTypeId)
+    ProductTypeId INT NOT NULL,
+    Price INT NOT NULL,
+    Name TEXT NOT NULL,
+    Description TEXT NOT NULL,
+    CustomerId INT NOT NULL,
+    FOREIGN KEY(CustomerId) REFERENCES Customers(CustomerId),
+    FOREIGN KEY(ProductTypeId) REFERENCES ProductTypes(ProductTypeId)
 );
 
 CREATE TABLE Customers (
     CustomerId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	FirstName TEXT NOT NULL,
+    FirstName TEXT NOT NULL,
     LastName TEXT NOT NULL,
-	PaymentTypeId INT NOT NULL,
-	AccountCreationDate TEXT NOT NULL,
-	DateOfLastActivity TEXT NOT NULL,
-	FOREIGN KEY (PaymentTypeId) REFERENCES PaymentTypes(PaymentTypeId)
+    PaymentTypeId INT NOT NULL,
+    AccountCreationDate TEXT NOT NULL,
+    DateOfLastActivity TEXT NOT NULL,
+    FOREIGN KEY (PaymentTypeId) REFERENCES PaymentTypes(PaymentTypeId)
 );
